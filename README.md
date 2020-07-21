9fc653c03124419c2b6b7c17f1311cf37315aa50
Yovach <yovach.mail@gmail.com>
2020-07-21 22:27:07 +0200

    refactor: remove unused imports, doc: some details were added

--- README.md
+++ README.md
@@ -10,17 +10,22 @@ This project uses the PaperMC API.
 
 ---
 
-## How it works ?
+## Setup your server
 
-### Setup your server
-
-To setup your server, you'll need to execute this command in your terminal :
+To setup your server on the latest version of Minecraft, you must run this command :
 
 ``deno run --allow-net --allow-write --allow-read https://raw.githubusercontent.com/Yovach/craftium/master/mod.ts --setup``
 
 
-### Update your server
+If you want to select your Minecraft version, please specify a version like this :
+``deno run --allow-net --allow-write --allow-read https://raw.githubusercontent.com/Yovach/craftium/master/mod.ts --setup=1.15.2``
 
-To setup your server, you'll need to execute this command in your terminal :
+---
+
+## Update your server
+
+**WARNING** : You'll need to setup before update.
+
+To update your server, you must run this command :
 
 ``deno run --allow-net --allow-write --allow-read https://raw.githubusercontent.com/Yovach/craftium/master/mod.ts --update``
--- mod.ts
+++ mod.ts
@@ -1,52 +1,60 @@
-import { PaperVersions, PaperLatestVersion } from "./src/paper.d.ts";
-import { downloadLatestVersion, paperAPI, fetchLatestVersion, checkUpdate, resetTmpDir, cleanup } from "./src/utils.ts";
+import { PaperVersions } from "./src/paper.d.ts";
+import {
+  downloadLatestVersion,
+  paperAPI,
+  checkUpdate,
+  resetTmpDir,
+  cleanup,
+} from "./src/utils.ts";
 
-import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { parse } from "https://deno.land/std/flags/mod.ts";
 const parsedArgs = parse(Deno.args);
 
 if (parsedArgs.setup) { // download and setup server files
-    
-    const paper = <PaperVersions>await paperAPI('/').then((res) => res.json())
-    let version = parsedArgs.setup;
-    const availableVersions = paper.versions;
-    if (typeof version === 'boolean' || !availableVersions.includes(version)) {
-        version = availableVersions[0]
-        console.warn('Version not specified or invalid, last version (' + version + ') selected.')
-    }
+  const paper = <PaperVersions> await paperAPI("/").then((res) => res.json());
+  let version = parsedArgs.setup;
+  const availableVersions = paper.versions;
+  if (typeof version === "boolean" || !availableVersions.includes(version)) {
+    version = availableVersions[0];
+    console.warn(
+      "Version not specified or invalid, last version (" + version +
+        ") selected.",
+    );
+  }
 
-    resetTmpDir();
+  resetTmpDir();
 
-    console.log(`Downloading paper.jar (${version})..`);
-    try {
-        // we create "server" directory if not exists
-        Deno.mkdirSync("./server/")
-    } catch (e) {
-        console.error(e.message)
-    }
-    await downloadLatestVersion(version);
-    console.log(`Downloading complete.`);
+  console.log(`Downloading paper.jar (${version})..`);
+  try {
+    // we create "server" directory if not exists
+    Deno.mkdirSync("./server/");
+  } catch (e) {
+    console.error(e.message);
+  }
+  await downloadLatestVersion(version);
+  console.log(`Downloading complete.`);
 } else if (parsedArgs.update) {
-    try {
-        Deno.statSync('paper.json');
-    } catch (e) {
-        if (e instanceof Deno.errors.NotFound) {
-            console.error("Please make a setup before update.");
-            Deno.exit(1);
-        } else {
-            throw e;
-        }
-    }
-    const updated = await checkUpdate();
-    if (updated) {
-        console.log('The new update was downloaded.')
+  try {
+    Deno.statSync("paper.json");
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      console.error("Please make a setup before update.");
+      Deno.exit(1);
     } else {
-        console.log('No updates were found.')
+      throw e;
     }
+  }
+  const updated = await checkUpdate();
+  if (updated) {
+    console.log("The new update was downloaded.");
+  } else {
+    console.log("No updates were found.");
+  }
 } else if (parsedArgs.cleanup) {
-    if (cleanup()) {
-        console.info('Temporary files has been removed') 
-    }
+  if (cleanup()) {
+    console.info("Temporary files has been removed");
+  }
 } else {
-    console.error("An error occured, please specify a valid instruction.")
-    Deno.exit(1)
+  console.error("An error occured, please specify a valid instruction.");
+  Deno.exit(1);
 }
